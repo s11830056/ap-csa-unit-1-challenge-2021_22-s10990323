bbbb52f17df769f5901b3999b983d1621921e4ee
Lorenzo Kenon <s10990323@students.duvalschools.org>
2021-10-22 00:48:27 -0400

    damage v0.1

---
+++ DamageDealt.java
@@ -0,0 +1,19 @@
+// <Student Name>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <Date>, <Time>, v0.0
+import java.util.Scanner;
+
+public class DamageDealt (String[] args) {
+    public static void main(string[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please enter UserName:");
+        String userName = myScanner.nextLine();
+        System.out.println("Hello fellow Hero!" + userName);
+
+        System.out.println("Please enter your weapon name:");
+        String weaponNanme = myScanner.nextLine();
+        String weaponName;
+        System.out.println(weaponName + "Is a wonderful choice");
+
+       
+    
+    }
+}
--- Unit1Challenge.java
+++
@@ -1,5 +0,0 @@
-// <Student Name>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <Date>, <Time>, v0.0
-
-public class ? {
-    
-}
