8ccd05618d711a65676424b5cce9168c0eb995d7
Lorenzo Kenon <s10990323@students.duvalschools.org>
2021-10-22 00:58:30 -0400

    v0.2

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,7 +1,7 @@
 // <Student Name>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <Date>, <Time>, v0.0
 import java.util.Scanner;
 
-public class DamageDealt (String[] args) {
+public class DamageDealt (String[] args); {
     public static void main(string[] args) {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Please enter UserName:");
@@ -12,6 +12,14 @@ public class DamageDealt (String[] args) {
         String weaponNanme = myScanner.nextLine();
         String weaponName;
         System.out.println(weaponName + "Is a wonderful choice");
+
+        System.out.println("Please enter the monster name:");
+        String monsterNanme = myScanner.nextLine();
+        String monsterName;
+        System.out.println(monsterName + "is a frightening monster!");
+
+        System.out.println("How many attacks would you like to ")
+
 
        
     
